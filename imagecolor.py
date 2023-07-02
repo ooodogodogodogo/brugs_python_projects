d7e88cde0db906c5ef73205c9fe22f60970bfe85
ooodogodogodogo <63647591+ooodogodogodogo@users.noreply.github.com>
2023-07-02 20:04:07 +0300

    Update imagecolor.py

--- imagecolor.py
+++ imagecolor.py
@@ -54,35 +54,3 @@ while maxcolor < len(maincolors):
 
 responselist = [dict([("id", idmap[key]),("hexcode",hexmap[key]) ,("colorname",key) ,("percentage",value)]) for (key,value) in maincolors.items()]
 print(json.dumps(responselist))
-
-"""
-for content in contents:
-    url = content[1]
-    try:
-        response = requests.get(url)
-        print(content[1])
-        # Load the image and get its dimensions
-        image = Image.open(BytesIO(response.content))
-        width, height = image.size
-
-        # Check the four corners of the image
-        p = 3
-        top_left = is_region_white(image, 0, 0, p, p)
-        top_right = is_region_white(image, width - p, 0, width, p)
-        bottom_left = is_region_white(image, 0, height - p, p, height)
-        bottom_right = is_region_white(image, width - p, height - p, width, height)
-        # Print the results
-        if (top_left and top_right) or (bottom_left and bottom_right):
-            results.append([content[0], content[1],"white_background"])
-        else:
-            results.append([content[0], content[1],"no_background"])
-            Break
-    except Exception:
-        pass
-
-with open('br_imagebackgrounds.csv', 'w', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    for result in results:
-        data = [result[0], result[1],result[2]]
-        writer.writerow(data)
-"""
