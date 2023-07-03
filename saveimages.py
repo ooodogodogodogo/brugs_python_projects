6cc4621f9aa7a31cab7714453a29bd6b5914f5fe
ooodogodogodogo <63647591+ooodogodogodogo@users.noreply.github.com>
2023-07-03 13:28:34 +0300

    Update saveimages.py

--- saveimages.py
+++ saveimages.py
@@ -3,33 +3,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 import csv
-"""
-def is_region_white(image, left, top, right, bottom):
-    rgbvals = image.convert('RGB')
-    #print(rgbvals.getpixel((left,top)))
-    for i in range(left, right):
-        for j in range(top,bottom):
-            r, g , b = rgbvals.getpixel((i,j))
-            print(r,g,b)
-            if  r<250 or g <250 or b <250:
-                return False
-    return True
 
-image = Image.open("no_background/no_background163.jpg")
-width, height = image.size
-
-# Check the four corners of the image
-p = 3
-top_left = is_region_white(image, 0, 0, p, p)
-top_right = is_region_white(image, width - p, 0, width, p)
-bottom_left = is_region_white(image, 0, height - p, p, height)
-bottom_right = is_region_white(image, width - p, height - p, width, height)
-# Print the results
-if (top_left and top_right) or (bottom_left and bottom_right):
-    print("white")
-else:
-    print("no")
-"""
 with open('br_imagebackgrounds_second.csv') as file:
     contents = file.readlines()
 contents = [content.replace('"', '').strip() for content in contents]
